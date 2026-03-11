3712bac98bc7a94dc0ba9facd3b21759a1ee38a5
Aldeine Riel A. Cruz <aldeineriel.cruz@neu.edu.ph>
2026-03-11 15:17:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2023894337-b9ed9",
+  "appId": "1:823579921587:web:1dc1afa04e9d79241834c6",
+  "apiKey": "AIzaSyAJra0DRH8o4B4kSbHURL1oF18q8ORnvXw",
+  "authDomain": "studio-2023894337-b9ed9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "823579921587"
+};
